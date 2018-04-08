73f288a35894a6d0ece6d2257bab82ef0e7d87f6
fengluxu <F.Xu-1@student.tudelft.nl>
2018-04-08 21:32:08 +0200

    modified

--- freehand_blend.m
+++ freehand_blend.m
@@ -1,95 +1,185 @@
-% This program creates an interactive application for gradient domain editing.
+% use the algirthm shown in the link below to blend
+%http://cs.brown.edu/courses/cs129/results/proj2/taox/
 
 clear all;
 close all;
-
-% Read in the source and target images:
+% read the image in the format of double precision 
 source = im2double(imread('source.jpeg'));
 target = im2double(imread('target.jpg'));
 
-[t_rows, t_cols, channels] = size(target);
+%show the image 
+imshow(source, []);
+%let user freehand select region they like
+freehand = imfreehand();
+fcn = makeConstrainToRectFcn('impoly',get(gca,'XLim'),get(gca,'YLim'));
+setPositionConstraintFcn(freehand,fcn);
+wait(freehand);
+% create binary mask for the freehand selection region
+mask = freehand.createMask();
 
-% Open an interactive session to allow the user to freehand select a region
-% of interest in the source image, and create a mask for that region:
-mask = create_freehand_mask(source);
-
-% We find the bounding box containing the nonzero values in the mask:
+% get non-zero rows and non-zero cols of mask
 [nonzero_rows, nonzero_cols] = find(mask);
+%find the min and max row/col
 min_row = min(nonzero_rows);
 max_row = max(nonzero_rows);
 min_col = min(nonzero_cols);
 max_col = max(nonzero_cols);
 
-width = max_col - min_col;
-height = max_row - min_row;
+%caculate the weight and height
+w = max_col - min_col;
+h = max_row - min_row;
 
-% We crop the source image and the mask itself to the size of the bounding
-% box, since the only pixels we care about from the source are those within
-% the mask:
+%crop the mask/region from source by bounding box
 source_cropped = source(min_row:max_row, min_col:max_col, :);
 mask_cropped = mask(min_row:max_row, min_col:max_col);
 
-%figure; imshow(source_cropped);
-%figure; imshow(mask_cropped);
 
-% We show the user the target image and allow him to drag and resize the 
-% rectangle representing the bounding box of the mask. This shows the user
-% the position and size of the copied patch from the source in the
-% destination image:
-figure; imshow(target);
-rect_handle = imrect(gca, [min_row min_col width height]);
-setFixedAspectRatioMode(rect_handle,1);
-wait(rect_handle);
-pos = rect_handle.getPosition()
+% select the region in the target
+figure; 
+imshow(target);
+rect = imrect(gca, [min_row min_col w h]);
+setFixedAspectRatioMode(rect,1);
+wait(rect);
+des = rect.getPosition()
 
-% Extract the components of the position:
-xmin = pos(1);
-ymin = pos(2);
-rect_cols = pos(3);
-rect_rows = pos(4);
+% get the size and offset of user-selected region in target image. 
+xmin = des(1);
+ymin = des(2);
+rect_cols = des(3);
+rect_rows = des(4);
 
-% Resize the source and mask if desired:
+%esize the cropped source and mask 
+%and pad them to be the same dimensions as the target image
 rf = imresize(source_cropped, [rect_rows, rect_cols]);
 rm = imresize(mask_cropped, [rect_rows, rect_cols]);
-
-% Pad the source and the mask to be the same dimensions as the target image
 source_padded = padarray(rf, [(size(target,1)-size(rf,1)), size(target,2)-size(rf,2)], 'post');
 mask_padded = padarray(rm, [(size(target,1)-size(rm,1)), size(target,2)-size(rm,2)], 'post');
 
-
-% Translate the source and target by the user-given (x,y) offset
+%translate the source and target rgarding to the offset
 source = imtranslate(source_padded, [xmin,ymin]);
 mask = imtranslate(mask_padded, [xmin,ymin]);
 
-% Now we find the new bounding box for the translated and resized region:
+%find the new bouding box with its width and height
 [nonzero_rows, nonzero_cols] = find(mask);
 min_row = min(nonzero_rows);
 max_row = max(nonzero_rows);
 min_col = min(nonzero_cols);
 max_col = max(nonzero_cols);
+w = max_col - min_col;
+h = max_row - min_row;
 
-width = max_col - min_col;
-height = max_row - min_row;
-
-% Now we crop all the images: the source, the mask, and the target, to
-% focus on the region that has to be blended:
+% corp the source, the mask, and the target based on the new bounding box
+%all of them have the same width and height
 source_region = source(min_row:max_row, min_col:max_col, :);
 mask_region = mask(min_row:max_row, min_col:max_col);
 target_region = target(min_row:max_row, min_col:max_col, :);
 
-% Show the naive copy-paste solution:
+%% direct paste 
 foreground = source .* repmat(mask, [1,1,3]);
 background = target .* repmat(~mask, [1,1,3]);
-direct_copy = background + foreground;
+direct_paste = background + foreground;
 figure;
-imshow(direct_copy);
+imshow(direct_paste);
 
-% Find the result of blending the cropped region:
-result = gradient_blend_samesize(source_region, mask_region, target_region);
+%% blending
+% use the algirthm shown in the link below
+%http://cs.brown.edu/courses/cs129/results/proj2/taox/
 
-% Insert the blended cropped region back into its place in the original
-% target, forming the final image:
-blended_image = target;
-blended_image(min_row:max_row, min_col:max_col, :) = result(:,:,:);
+%add a 1-pixel border around the source,target, and mask.
+%will be removed after blending. 
+source_region = padarray(source_region, [1,1], 'symmetric');
+target_region = padarray(target_region, [1,1], 'symmetric');
+mask_region = padarray(mask_region, [1,1]);
 
-figure; imshow(blended_image);
+%get the size of target region
+[rows, cols, ~] = size(target_region);
+
+s = reshape(source_region, rows*cols, []);
+t = reshape(target_region, rows*cols, []);
+
+% initialize vector b
+b = zeros(rows*cols, 3);
+%row_vec has entries that represent row indexes of A
+row_vec = zeros(rows*cols, 1);
+%col_vec has entries that represent column indexes of A
+col_vec = zeros(rows*cols, 1);
+%value_vec stores values at specific positions inside A. 
+value_vec = zeros(rows*cols, 1);
+
+%track the current row sparse matrix A
+eq_n = 1;
+
+% iterate through rows of matrix A, 
+%insert the appropriate values in the matrix A and b
+for i = 1:rows*cols
+    % If the current pixel location is in the mask
+    if mask_region(i)
+        b(i,:) = 4*s(i,:) - s(i-1,:) - s(i+1,:) - s(i+rows,:) - s(i-rows,:);
+        
+        % Insert a 4 into A at the i of the current central pixel.
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i;
+        value_vec(eq_n) = 4;
+        eq_n = eq_n + 1;
+        
+        %Insert a -1 for the pixel to four neighbours of the current pixel 
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i + 1;
+        value_vec(eq_n) = -1;
+        eq_n = eq_n + 1;
+
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i - 1;
+        value_vec(eq_n) = -1;
+        eq_n = eq_n + 1;
+
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i - rows;
+        value_vec(eq_n) = -1;
+        eq_n = eq_n + 1;
+           
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i + rows;
+        value_vec(eq_n) = -1;
+        eq_n = eq_n + 1;
+    else
+        % If the current pixel location is not in the mask
+        row_vec(eq_n) = i;
+        col_vec(eq_n) = i;
+        value_vec(eq_n) = 1;
+        eq_n = eq_n + 1;
+        
+        b(i,:) = t(i,:);
+    end
+end
+
+%create the sparse matrix 
+A = sparse(row_vec, col_vec, value_vec, rows*cols, rows*cols);
+
+toc
+disp('Finished constructing the matrix A...')
+
+% caculate the result for each channel
+c_red = A \ b(:,1);
+c_green = A \ b(:,2);
+c_blue = A \ b(:,3);
+
+% reshape to the original size
+c_red = reshape(c_red, [rows, cols]);
+c_green = reshape(c_green, [rows, cols]);
+c_blue = reshape(c_blue, [rows, cols]);
+
+% stack the channels back
+result = zeros(rows, cols, 3);
+result(:,:,1) = c_red;
+result(:,:,2) = c_green;
+result(:,:,3) = c_blue;
+
+% remove the border:
+result = result(2:rows-1, 2:cols-1, :);
+%% show the final blending result
+b_image = target;
+b_image(min_row:max_row, min_col:max_col, :) = result(:,:,:);
+
+figure; 
+imshow(b_image);
